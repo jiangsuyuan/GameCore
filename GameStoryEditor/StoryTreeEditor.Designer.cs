4fff773449f175888bcd24fa6db3c924b6e1a18a
苏原 姜 <jiangsuyuan0000@sina.com>
2020-05-19 18:43:24 +0800

    Commit

--- GameStoryEditor/StoryTreeEditor.Designer.cs
+++ GameStoryEditor/StoryTreeEditor.Designer.cs
@@ -61,6 +61,8 @@
             this.label9 = new System.Windows.Forms.Label();
             this.label10 = new System.Windows.Forms.Label();
             this.label11 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -79,7 +81,7 @@
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Location = new System.Drawing.Point(116, 6);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(168, 35);
+            this.comboBox1.Size = new System.Drawing.Size(168, 29);
             this.comboBox1.TabIndex = 2;
             // 
             // dataGridView1
@@ -101,7 +103,7 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(688, 59);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(112, 27);
+            this.label2.Size = new System.Drawing.Size(90, 21);
             this.label2.TabIndex = 5;
             this.label2.Text = "性别要求：";
             // 
@@ -111,7 +113,7 @@
             this.label4.AutoSize = true;
             this.label4.Location = new System.Drawing.Point(688, 109);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(112, 27);
+            this.label4.Size = new System.Drawing.Size(90, 21);
             this.label4.TabIndex = 5;
             this.label4.Text = "时间下限：";
             // 
@@ -121,7 +123,7 @@
             this.label5.AutoSize = true;
             this.label5.Location = new System.Drawing.Point(688, 154);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(112, 27);
+            this.label5.Size = new System.Drawing.Size(90, 21);
             this.label5.TabIndex = 5;
             this.label5.Text = "好感下限：";
             // 
@@ -131,7 +133,7 @@
             this.label6.AutoSize = true;
             this.label6.Location = new System.Drawing.Point(688, 196);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(112, 27);
+            this.label6.Size = new System.Drawing.Size(90, 21);
             this.label6.TabIndex = 5;
             this.label6.Text = "类型下限：";
             // 
@@ -141,7 +143,7 @@
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(688, 14);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(112, 27);
+            this.label3.Size = new System.Drawing.Size(90, 21);
             this.label3.TabIndex = 5;
             this.label3.Text = "剧情名称：";
             // 
@@ -149,9 +151,9 @@
             // 
             this.radioButton1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(806, 58);
+            this.radioButton1.Location = new System.Drawing.Point(815, 58);
             this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(53, 31);
+            this.radioButton1.Size = new System.Drawing.Size(44, 25);
             this.radioButton1.TabIndex = 6;
             this.radioButton1.Text = "男";
             this.radioButton1.UseVisualStyleBackColor = true;
@@ -160,9 +162,9 @@
             // 
             this.radioButton2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(865, 58);
+            this.radioButton2.Location = new System.Drawing.Point(874, 58);
             this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(53, 31);
+            this.radioButton2.Size = new System.Drawing.Size(44, 25);
             this.radioButton2.TabIndex = 6;
             this.radioButton2.Text = "女";
             this.radioButton2.UseVisualStyleBackColor = true;
@@ -172,9 +174,9 @@
             this.radioButton3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.radioButton3.AutoSize = true;
             this.radioButton3.Checked = true;
-            this.radioButton3.Location = new System.Drawing.Point(924, 58);
+            this.radioButton3.Location = new System.Drawing.Point(937, 58);
             this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(73, 31);
+            this.radioButton3.Size = new System.Drawing.Size(60, 25);
             this.radioButton3.TabIndex = 6;
             this.radioButton3.TabStop = true;
             this.radioButton3.Text = "不限";
@@ -185,7 +187,7 @@
             this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.textBox2.Location = new System.Drawing.Point(806, 11);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(424, 34);
+            this.textBox2.Size = new System.Drawing.Size(424, 29);
             this.textBox2.TabIndex = 7;
             // 
             // textBox3
@@ -193,7 +195,7 @@
             this.textBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.textBox3.Location = new System.Drawing.Point(806, 105);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(147, 34);
+            this.textBox3.Size = new System.Drawing.Size(147, 29);
             this.textBox3.TabIndex = 7;
             this.textBox3.Text = "0";
             // 
@@ -202,7 +204,7 @@
             this.textBox4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.textBox4.Location = new System.Drawing.Point(1083, 104);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(147, 34);
+            this.textBox4.Size = new System.Drawing.Size(147, 29);
             this.textBox4.TabIndex = 7;
             this.textBox4.Text = "9999999";
             // 
@@ -211,7 +213,7 @@
             this.textBox5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.textBox5.Location = new System.Drawing.Point(806, 149);
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(147, 34);
+            this.textBox5.Size = new System.Drawing.Size(147, 29);
             this.textBox5.TabIndex = 7;
             this.textBox5.Text = "-100";
             // 
@@ -220,7 +222,7 @@
             this.textBox6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.textBox6.Location = new System.Drawing.Point(1083, 149);
             this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(147, 34);
+            this.textBox6.Size = new System.Drawing.Size(147, 29);
             this.textBox6.TabIndex = 7;
             this.textBox6.Text = "100";
             // 
@@ -229,7 +231,7 @@
             this.textBox7.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.textBox7.Location = new System.Drawing.Point(806, 192);
             this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(147, 34);
+            this.textBox7.Size = new System.Drawing.Size(147, 29);
             this.textBox7.TabIndex = 7;
             this.textBox7.Text = "-100";
             // 
@@ -238,7 +240,7 @@
             this.textBox8.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.textBox8.Location = new System.Drawing.Point(1083, 191);
             this.textBox8.Name = "textBox8";
-            this.textBox8.Size = new System.Drawing.Size(147, 34);
+            this.textBox8.Size = new System.Drawing.Size(147, 29);
             this.textBox8.TabIndex = 7;
             this.textBox8.Text = "100";
             // 
@@ -288,11 +290,11 @@
             // 
             this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox1.Location = new System.Drawing.Point(116, 614);
+            this.textBox1.Location = new System.Drawing.Point(176, 614);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox1.Size = new System.Drawing.Size(955, 100);
+            this.textBox1.Size = new System.Drawing.Size(895, 100);
             this.textBox1.TabIndex = 3;
             // 
             // button5
@@ -320,9 +322,9 @@
             // 
             this.checkBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(1115, 59);
+            this.checkBox1.Location = new System.Drawing.Point(1135, 59);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(115, 31);
+            this.checkBox1.Size = new System.Drawing.Size(95, 25);
             this.checkBox1.TabIndex = 8;
             this.checkBox1.Text = "NPC先手";
             this.checkBox1.UseVisualStyleBackColor = true;
@@ -331,11 +333,11 @@
             // 
             this.textBox9.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox9.Location = new System.Drawing.Point(116, 510);
+            this.textBox9.Location = new System.Drawing.Point(176, 510);
             this.textBox9.Multiline = true;
             this.textBox9.Name = "textBox9";
             this.textBox9.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox9.Size = new System.Drawing.Size(955, 100);
+            this.textBox9.Size = new System.Drawing.Size(895, 100);
             this.textBox9.TabIndex = 3;
             // 
             // label1
@@ -345,7 +347,7 @@
             this.label1.Location = new System.Drawing.Point(-3, 513);
             this.label1.MaximumSize = new System.Drawing.Size(120, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(72, 27);
+            this.label1.Size = new System.Drawing.Size(58, 21);
             this.label1.TabIndex = 5;
             this.label1.Text = "当前：";
             // 
@@ -354,7 +356,7 @@
             this.label7.AutoSize = true;
             this.label7.Location = new System.Drawing.Point(-3, 9);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(113, 27);
+            this.label7.Size = new System.Drawing.Size(92, 21);
             this.label7.TabIndex = 5;
             this.label7.Text = "选择NPC：";
             // 
@@ -365,7 +367,7 @@
             this.label8.Location = new System.Drawing.Point(-2, 614);
             this.label8.MaximumSize = new System.Drawing.Size(120, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(92, 27);
+            this.label8.Size = new System.Drawing.Size(74, 21);
             this.label8.TabIndex = 5;
             this.label8.Text = "新回应：";
             // 
@@ -375,7 +377,7 @@
             this.label9.AutoSize = true;
             this.label9.Location = new System.Drawing.Point(965, 109);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(112, 27);
+            this.label9.Size = new System.Drawing.Size(90, 21);
             this.label9.TabIndex = 5;
             this.label9.Text = "时间上限：";
             // 
@@ -385,7 +387,7 @@
             this.label10.AutoSize = true;
             this.label10.Location = new System.Drawing.Point(965, 154);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(112, 27);
+            this.label10.Size = new System.Drawing.Size(90, 21);
             this.label10.TabIndex = 5;
             this.label10.Text = "好感上限：";
             // 
@@ -395,13 +397,29 @@
             this.label11.AutoSize = true;
             this.label11.Location = new System.Drawing.Point(965, 196);
             this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(112, 27);
+            this.label11.Size = new System.Drawing.Size(90, 21);
             this.label11.TabIndex = 5;
             this.label11.Text = "类型上限：";
             // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(2, 547);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(168, 29);
+            this.comboBox2.TabIndex = 2;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(2, 651);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(168, 29);
+            this.comboBox3.TabIndex = 2;
+            // 
             // StoryTreeEditor
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 27F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1236, 723);
             this.Controls.Add(this.checkBox1);
@@ -429,6 +447,8 @@
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.textBox9);
             this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
@@ -484,6 +504,8 @@
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Label label10;
         private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
     }
 }
 
